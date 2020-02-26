01f5ffc77355ca6a4b3eb876c6b2ab6e624f3595
Corey Brown <coreybrown89@gmail.com>
2020-02-26 01:35:11 -0600

    adds about page

---
+++ src/pages/vision.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+function Vision() {
+  return (
+    <Layout>
+      <main>
+        <div class="hero">
+          <div class="container">
+            <h1 class="hero__title">About</h1>
+            <p class="hero__subtitle">The vision &amp; philosophy.</p>
+            <p>Coming soon.</p>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+}
+
+Vision.title = "Vision &amp; Philosophy";
+Vision.description = "The vision and philosophy of Blitz";
+
+export default Vision;

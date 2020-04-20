55e1535facd202190633d348e5c200497750bc5c
Corey Brown <coreybrown89@gmail.com>
2020-04-19 19:43:53 -0500

    updates github edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -85,7 +85,7 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://github.com/facebook/docusaurus/edit/master/website/"
+          editUrl: "https://github.com/blitz-js/blitzjs.com/tree/master/"
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css")

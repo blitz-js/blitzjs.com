9fce3d47cceaef176d8dcdbd05f0fca240e926d1
robdrosenberg <robertdr85@gmail.com>
2020-05-11 22:26:18 -0700

    adds theme ui package and intial theme.js

---
+++ src/theme/theme.js
@@ -0,0 +1,35 @@
+export default {
+  fonts: {
+    body:
+      'Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", sans-serif',
+    heading: '"Libre-franklin", serif',
+    monospace: "Menlo, monospace",
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  letterSpacings: {
+    body: "normal",
+    caps: "0.2em",
+  },
+  colors: {
+    text: "#F4F4F4",
+    background: "#45009D",
+    primary: "##6700EB",
+    secondary: "#45009D",
+  },
+  styles: {
+    root: {
+      // uses the theme values provided above
+      fontFamily: "body",
+      fontWeight: "body",
+    },
+  },
+};

9b390eb6a4780b4b5263265624ed5f34a1c3298d
Brandon Bayer <b@bayer.ws>
2020-06-03 19:37:02 +0700

    Add all documentation from Next.js (#70)

--- src/components/DocItem/index.js
+++ src/components/DocItem/index.js
@@ -123,25 +123,14 @@ function DocItem(props) {
                 <div className="margin-vert--md">
                   <div className="row">
                     <div className="col">
-                      {editUrl && (
-                        <a href={editUrl} target="_blank" rel="noreferrer noopener">
-                          <svg
-                            fill="currentColor"
-                            height="1.2em"
-                            width="1.2em"
-                            preserveAspectRatio="xMidYMid meet"
-                            viewBox="0 0 40 40"
-                            style={{
-                              marginRight: "0.3em",
-                              verticalAlign: "sub",
-                            }}>
-                            <g>
-                              <path d="m34.5 11.7l-3 3.1-6.3-6.3 3.1-3q0.5-0.5 1.2-0.5t1.1 0.5l3.9 3.9q0.5 0.4 0.5 1.1t-0.5 1.2z m-29.5 17.1l18.4-18.5 6.3 6.3-18.4 18.4h-6.3v-6.2z" />
-                            </g>
-                          </svg>
-                          Edit this page on Github
-                        </a>
-                      )}
+                      Idea for improving this page?
+                      <a
+                        href={editUrl}
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        sx={{ml: 2}}>
+                        Edit it on Github
+                      </a>
                     </div>
                     {(lastUpdatedAt || lastUpdatedBy) && (
                       <div className="col text--right">

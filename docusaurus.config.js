ea6976c4f7b3b4319eac1969d395873bb0415ede
Daniel Molnar <dm@tpwd.de>
2020-07-04 04:37:55 +0200

    Update docusaurus.config.js (#109)
    - replaced 'master' in edit link with 'main'

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -118,7 +118,7 @@ module.exports = {
       "@docusaurus/plugin-content-docs",
       {
         sidebarPath: require.resolve("./sidebars.js"),
-        editUrl: "https://github.com/blitz-js/blitzjs.com/edit/master/",
+        editUrl: "https://github.com/blitz-js/blitzjs.com/edit/main/",
       },
     ],
     ["@docusaurus/plugin-content-blog"],

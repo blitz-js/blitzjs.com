84ed3fbcad033c5dca8183ce1d5570bcdc24706b
Brandon Bayer <b@bayer.ws>
2020-11-23 14:25:42 -0500

    add doc page using fauna

--- sidebars.js
+++ sidebars.js
@@ -43,7 +43,7 @@ module.exports = {
       "router",
       "api-routes",
     ],
-    Database: ["database-overview", "database-seeds"],
+    Database: ["database-overview", "database-seeds", "fauna"],
     Queries: [
       "query-resolvers",
       "query-usage",

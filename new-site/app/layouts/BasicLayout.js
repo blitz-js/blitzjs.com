386908489db3f8a15c04204c6978429587517d62
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-01-08 10:34:56 +0800

    Update BasicLayout, use Header w current container

--- new-site/app/layouts/BasicLayout.js
+++ new-site/app/layouts/BasicLayout.js
@@ -1,6 +1,22 @@
-import Header from "@/components/Header"
+import { Header } from "@/components/Header"
 import React from "react"
 
 export function BasicLayout({ children, navIsOpen, setNavIsOpen }) {
-  return children
+  return (
+    <div>
+      <div className="overflow-hidden">
+        <div>
+          <a name="top" aria-hidden>
+            {null}
+          </a>
+          <div className="relative grid grid-cols-container gap-y-24">
+            <div className="col-start-2 text-white">
+              <Header />
+            </div>
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }

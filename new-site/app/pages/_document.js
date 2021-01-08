54d44bb1910af1cf631c8b2e011f26eca3b8a833
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-01-08 12:58:34 +0800

    Add existing blitz favicons

--- new-site/app/pages/_document.js
+++ new-site/app/pages/_document.js
@@ -12,7 +12,7 @@ export default class Document extends NextDocument {
       <Html lang="en" className="antialiased text-black bg-white">
         <Head>
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/32x32-Favicon-Purple.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00b4b6" />

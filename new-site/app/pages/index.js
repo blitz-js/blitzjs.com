ee7f281c57beaf5da180c019d4b938d4418035b7
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-01-10 00:26:53 +0800

    Remove footer markup and unused components

--- new-site/app/pages/index.js
+++ new-site/app/pages/index.js
@@ -10,10 +10,7 @@ import { Feature } from "@/components/home/Feature"
 import { Octokit } from "@octokit/rest"
 import { FeatureIcon } from "@/components/home/FeatureIcon"
 import { Sponsor } from "@/components/home/Sponsor"
-import { LinkList } from "@/components/home/LinkList"
-import { NewsletterForm } from "@/components/home/NewsletterForm"
 import { FaGithub } from "react-icons/fa"
-import { Icon } from "../components/home/Icon"
 import { BasicLayout } from "../layouts/BasicLayout"
 
 const Home = ({ randomContributors }) => {
@@ -257,58 +254,6 @@ const Home = ({ randomContributors }) => {
           <ButtonLink href="#" className="py-18 rounded-br-2xl">
             Releases
           </ButtonLink>
-        </div>
-      </div>
-
-      <div className="relative grid col-span-3 text-white border-t border-white border-opacity-50 grid-cols-container gap-y-7">
-        <a href="#top">
-          <Icon
-            name="arrowUp"
-            className="absolute right-0 mr-2 -mt-5"
-            size="2.5rem"
-            iconSize="1.8rem"
-          ></Icon>
-        </a>
-        <div className="col-span-3"></div>
-        <p className="col-start-2 text-lg font-semibold">
-          Want to receive the latest news and updates from the Blitz team? Sign up for our
-          newsletter!
-        </p>
-        <NewsletterForm className="col-start-2 mb-4" />
-        <LinkList title="Docs" className="col-start-2">
-          <Link href="#">
-            <a>Getting Started</a>
-          </Link>
-          <Link href="#">
-            <a>Contributing</a>
-          </Link>
-        </LinkList>
-
-        <LinkList title="Community" className="col-start-2">
-          <Link href="#">
-            <a>Slack</a>
-          </Link>
-          <Link href="#">
-            <a>Forum Discussions</a>
-          </Link>
-          <Link href="#">
-            <a>Donate/Sponsor</a>
-          </Link>
-        </LinkList>
-
-        <LinkList title="Social" className="col-start-2">
-          <Link href="#">
-            <a>GitHub</a>
-          </Link>
-          <Link href="#">
-            <a>Twitter</a>
-          </Link>
-        </LinkList>
-
-        <div className="col-start-2 mb-3 text-xs font-secondary">
-          Hosted on Vercel
-          <br />
-          Copyright &copy; 2020 Brandon Bayer and Blitz.js Contributors
         </div>
       </div>
     </>

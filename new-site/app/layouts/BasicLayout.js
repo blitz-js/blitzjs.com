60d57b60e75e59cc99cbf0bed66ef743c3c927a8
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-01-11 16:49:07 +0800

    Revert changes to BasicLayout

--- new-site/app/layouts/BasicLayout.js
+++ new-site/app/layouts/BasicLayout.js
@@ -1,24 +1,6 @@
-import { Header } from "@/components/Header"
-import { Footer } from "@/components/home/Footer"
+import Header from "@/components/Header"
 import React from "react"
 
 export function BasicLayout({ children, navIsOpen, setNavIsOpen }) {
-  return (
-    <div>
-      <div className="overflow-hidden">
-        <div>
-          <a name="top" aria-hidden>
-            {null}
-          </a>
-          <div className="relative grid grid-cols-1 py-1 md:py-3 gap-y-24 xl:gap-y-36">
-            <div className="text-white col-span-full">
-              <Header className="px-6 mx-auto max-w-7xl" />
-            </div>
-            {children}
-            <Footer />
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+  return children
 }

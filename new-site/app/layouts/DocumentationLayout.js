9b21a4d0d8551535093c2515c7774deeef2e1742
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-01-19 13:59:24 +0800

    Remove nav from doc layout

--- new-site/app/layouts/DocumentationLayout.js
+++ new-site/app/layouts/DocumentationLayout.js
@@ -3,7 +3,6 @@ import Head from "next/head"
 import { useRouter } from "next/router"
 import twitterSquare from "@/img/twitter-square.jpg"
 import { Title } from "@/components/Title"
-import { documentationNav } from "@/navs/documentation"
 import { Header } from "@/components/Header"
 import { Footer } from "@/components/home/Footer"
 
@@ -27,7 +26,7 @@ export function DocumentationLayout(props) {
         className="flex items-center justify-between px-6 mx-auto max-w-7xl"
         bannerMsg="Blitz is now in beta!"
       />
-      <SidebarLayout nav={documentationNav} {...props} />
+      <SidebarLayout {...props} />
       <Footer className="text-black dark:text-white" />
     </div>
   )

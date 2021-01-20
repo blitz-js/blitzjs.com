06c89d082416ee9d71dd893f9fee50ea04e566b9
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-01-20 15:05:18 +0800

    Update prev/next hook for new docs nav object

--- new-site/app/hooks/usePrevNext.js
+++ new-site/app/hooks/usePrevNext.js
@@ -5,7 +5,7 @@ import { useRouter } from "next/router"
 export function usePrevNext() {
   let router = useRouter()
   let { nav } = useContext(SidebarContext)
-  let pages = Object.keys(nav).flatMap((category) => nav[category])
+  let pages = nav.flatMap((category) => category.pages)
   let pageIndex = pages.findIndex((page) => page.href === router.pathname)
   return {
     prev: pageIndex > -1 ? pages[pageIndex - 1] : undefined,

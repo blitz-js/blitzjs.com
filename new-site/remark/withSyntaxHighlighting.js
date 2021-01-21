2d672f7679e49c2aa58d9a991e14ab8d0dbfab98
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-01-21 13:41:01 +0800

    Update code block styling

--- new-site/remark/withSyntaxHighlighting.js
+++ new-site/remark/withSyntaxHighlighting.js
@@ -1,35 +1,22 @@
 const visit = require("unist-util-visit")
 const { highlightCode } = require("./utils")
 
-const colors = {
-  amber: "bg-amber-500",
-  emerald: "bg-emerald-500",
-  fuchsia: "bg-fuchsia-400",
-  indigo: "bg-indigo-400",
-  lightBlue: "bg-light-blue-500",
-  purple: "bg-purple-400",
-  rose: "bg-rose-400",
-}
-
 module.exports.withSyntaxHighlighting = () => {
   return (tree) => {
     visit(tree, "code", (node) => {
-      if (node.lang !== null) {
-        node.type = "html"
-        node.value = [
-          `<div class="my-6 rounded-xl overflow-hidden ${colors[node.meta] || "bg-gray-800"}">`,
-          `<pre class="language-${node.lang} ${
-            colors[node.meta] ? "bg-black bg-opacity-75" : ""
-          }">`,
-          `<code class="language-${node.lang}">`,
-          highlightCode(node.value, node.lang),
-          "</code>",
-          "</pre>",
-          "</div>",
-        ]
-          .filter(Boolean)
-          .join("")
-      }
+      const lang = node.lang || "bash"
+      node.type = "html"
+      node.value = [
+        `<div class="my-6 rounded-xl overflow-hidden bg-code-block dark:bg-purple-off-black">`,
+        `<pre class="language-${lang}">`,
+        `<code class="language-${lang} text-black dark:text-white">`,
+        highlightCode(node.value, lang),
+        "</code>",
+        "</pre>",
+        "</div>",
+      ]
+        .filter(Boolean)
+        .join("")
     })
   }
 }

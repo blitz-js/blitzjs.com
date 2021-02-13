936c351d6e548efda9be34615a534d2bde210cef
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-02-13 08:36:52 +0800

    Update pageheader style for mobile

--- new-site/app/components/PageHeader.js
+++ new-site/app/components/PageHeader.js
@@ -2,9 +2,11 @@ export function PageHeader({ title }) {
   if (!title) return null
 
   return (
-    <div className="pb-10">
+    <div className="lg:pb-10">
       <div className="flex items-center">
-        <h1 className="text-5xl font-semibold text-black dark:text-white font-primary">{title}</h1>
+        <h1 className="text-3xl lg:text-5xl font-semibold text-black dark:text-white font-primary">
+          {title}
+        </h1>
       </div>
     </div>
   )

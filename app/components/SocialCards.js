900eb7c4bd5e91825b1025e79a4d269391c5d557
Brandon Bayer <b@bayer.ws>
2021-02-25 10:15:18 -0500

    fix twitter cards

--- app/components/SocialCards.js
+++ app/components/SocialCards.js
@@ -3,8 +3,8 @@ import Head from "next/head"
 export const SocialCards = ({ imageUrl }) => {
   return (
     <Head>
-      <meta key="twitter:image" name="twitter:image" content={imageUrl} />
-      <meta key="og:image" property="og:image" content={imageUrl} />
+      <meta key="twitter:image" name="twitter:image" content={"https://blitzjs.com" + imageUrl} />
+      <meta key="og:image" property="og:image" content={"https://blitzjs.com" + imageUrl} />
     </Head>
   )
 }

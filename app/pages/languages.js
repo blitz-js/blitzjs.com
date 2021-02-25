75518c46c4ede81d8a72ecaa76919d528e1318f1
JuanM04 <me@juanm04.com>
2021-02-25 12:47:09 -0300

    Languages and Translations entry (#387)
    Co-authored-by: Brandon Bayer <b@bayer.ws>

---
+++ app/pages/languages.js
@@ -0,0 +1,129 @@
+import { Header } from "@/components/Header"
+import { Octokit } from "@octokit/rest"
+import { Footer } from "@/components/home/Footer"
+import { useState, useEffect } from "react"
+import { SocialCards } from "../components/SocialCards"
+
+const LanguagesPage = ({ languages }) => {
+  const [navIsOpen, setNavIsOpen] = useState(false)
+
+  useEffect(() => {
+    document.body.style.overflow = navIsOpen ? "hidden" : "unset"
+  }, [navIsOpen])
+
+  return (
+    <div className="relative py-1 md:py-3 min-h-screen bg-white dark:bg-purple-deep">
+      <SocialCards imageUrl="/social-homepage.png" />
+      <Header
+        className="px-6 mx-auto max-w-7xl"
+        hasLightBg
+        useColoredLogo
+        stickyBgClass="bg-white dark:bg-purple-deep"
+        hasFade
+        onNavToggle={(isOpen) => {
+          setNavIsOpen(isOpen)
+        }}
+      />
+      <div
+        className={
+          "absolute w-full h-full row-start-1 row-end-5 background-to-video rounded-bl-3xl xl:rounded-bl-4xl bg-gradient-to-b from-purple-mid to-purple-primary dark:from-black dark:to-purple-off-black " +
+          (navIsOpen ? "z-20 fixed" : "-z-10")
+        }
+      ></div>
+      <main className="mx-auto max-w-7xl px-6 py-24 xl:py-44 text-black dark:text-dark-mode-text space-y-16 lg:space-y-20">
+        <div className="space-y-6">
+          <h1 className="font-primary text-3xl lg:text4xl xl:text-5xl font-semibold">Languages</h1>
+          <p className="font-secondary text-lg text-gray-600 dark:text-gray-300">
+            The Blitz documentation is currently being translated into the following languages:
+          </p>
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-x-12 gap-y-6">
+          {languages.map((lang) => (
+            <div key={lang.code}>
+              <h3 className="xl:mb-0 text-xl">{lang.name}</h3>
+              <p className="text-sm font-secondary">
+                {lang.completition}% —{" "}
+                <a
+                  href={`https://github.com/blitz-js/${lang.code}.blitzjs.com/issues/1`}
+                  className="text-purple-light dark:text-purple-extralight font-medium dark:font-bold no-underline dark:underline hover:underline"
+                >
+                  Help Translate
+                </a>
+              </p>
+            </div>
+          ))}
+        </div>
+      </main>
+      <Footer className="text-black dark:text-dark-mode-text" hasDarkMode />
+    </div>
+  )
+}
+
+const getStaticProps = async () => {
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_AUTH_TOKEN,
+  })
+
+  // Theorically, this will break when we reach 1000+ languages
+  const { data } = await octokit.repos.getContent({
+    owner: "blitz-js",
+    repo: "blitzjs.com-translation",
+    path: "langs",
+  })
+
+  const languages = await Promise.all(
+    data.map(async (lang) => {
+      const [{ data: langJson }, { data: langIssue }] = await Promise.all([
+        // Gets each lang.json content, because it doesn't come with the first request `data`
+        octokit.repos.getContent({
+          owner: "blitz-js",
+          repo: "blitzjs.com-translation",
+          path: lang.path,
+        }),
+        octokit.issues.get({
+          owner: "blitz-js",
+          repo: `${lang.name.substr(0, lang.name.length - 5)}.blitzjs.com`,
+          issue_number: 1,
+        }),
+      ])
+
+      const langMeta = JSON.parse(
+        Buffer.from(langJson.content, langJson.encoding).toString("utf-8")
+      )
+
+      const checkedBoxes = langIssue.body.match(/\* \[x\]/gi)
+      const totalBoxes = langIssue.body.match(/\* \[(x| )?\]/gi)
+
+      // Instead of returning an empty array when there aren't any matches, `match` returns `undefined`,
+      // so this checks for empty arrays (and prevents dividing by 0)
+      const completition = !totalBoxes
+        ? 100
+        : !checkedBoxes
+        ? 0
+        : Math.round((checkedBoxes.length / totalBoxes.length) * 100)
+
+      return { ...langMeta, completition }
+    })
+  )
+
+  return {
+    props: {
+      languages: languages.sort((a, b) =>
+        a.completition === b.completition
+          ? a.name.localeCompare(b.name)
+          : a.completition > b.completition
+      ),
+    },
+    revalidate: 3 * 60 * 60, // 3 hours
+  }
+}
+
+LanguagesPage.layoutProps = {
+  meta: {
+    title: "Languages - Blitz.js",
+    description: `Blitz is a hyper-productive fullstack React framework that's built on Next.js and features a "Zero-API" data layer.`,
+  },
+}
+
+export default LanguagesPage
+export { getStaticProps }

4f1f4e9659bacf3e3068433ccc6c563a4171f0da
Brandon Bayer <b@bayer.ws>
2021-03-24 10:58:12 -0400

    Add Digsas as sponsor

--- app/core/components/SponsorPack.js
+++ app/core/components/SponsorPack.js
@@ -42,6 +42,14 @@ const sponsors = [
     tier: 4,
     cost: 100,
   },
+  {
+    name: "Digsas",
+    href:
+      "https://digsas.com/?utm_source=blitzjs&utm_medium=sponsorship&utm_campaign=blitzjs_sponsorship",
+    imageUrl: "https://raw.githubusercontent.com/blitz-js/blitz/canary/assets/digsas.svg",
+    tier: 4,
+    cost: 100,
+  },
 ]
 
 const pack = {

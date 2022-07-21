73aa6d5cb4214d87e6a2b2c0076ca1124904dc92
Brandon Bayer <b@bayer.ws>
2022-07-21 19:59:57 +0100

    cleanup

--- app/pages/index.js
+++ app/pages/index.js
@@ -146,7 +146,7 @@ const Home = ({randomContributors}) => {
                   <div className="grid pb-4 ml-6 features-grid lg:gap-y-14">
                     <Feature title="Built for Monoliths and Services">
                       <p>
-                        You can everything from the database to your frontend all inside a single
+                        You can manage everything from the database to your frontend all inside a single
                         app. Only one thing to develop. Only one thing to deploy.
                       </p>
                       <p>Or not. Your choice.</p>
